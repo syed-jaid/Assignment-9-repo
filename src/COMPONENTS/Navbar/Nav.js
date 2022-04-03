4f905327549ed95aba25cebacb520350a5a008e3
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-04-04 03:43:46 +0600

    navbar is done and boostarp cdn link is conected

--- src/COMPONENTS/Navbar/Nav.js
+++ src/COMPONENTS/Navbar/Nav.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Nav.css'
 const Nav = () => {
     return (
-        <div>
-            <h1>nav</h1>
-            <Link to='/'>Home</Link>
-            <Link to='/reviews'>Reviews</Link>
-            <Link to='/dashboard'>Dashborad</Link>
-            <Link to='/blogs'>Blogs</Link>
+        <div className='nav-main-div'>
+            {/* all links for route */}
+            <Link className='nav-links' to='/'>Home</Link>
+            <Link className='nav-links' to='/reviews'>Reviews</Link>
+            <Link className='nav-links' to='/dashboard'>Dashborad</Link>
+            <Link className='nav-links' to='/blogs'>Blogs</Link>
         </div>
     );
 };
