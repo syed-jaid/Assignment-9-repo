ecef6044dc8e1fba2fdbbf98f8e913a830e373e0
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-04-04 05:40:20 +0600

    half of home section part is done

--- src/COMPONENTS/Home/Home.js
+++ src/COMPONENTS/Home/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Home.css'
 import Nav from '../Navbar/Nav';
-
+import img from '../img/mfile_1628229_1_L_20220110223616.jpg'
 const Home = () => {
     return (
-        <div>
+        <div className='container'>
             <Nav></Nav>
-            <h1>home</h1>
+            {/* main prat */}
+            <div className='sub-div d-flex'>
+                {/* home title  */}
+                <div >
+                    <h1 className='home-title home-title-color'>Find The Best Car</h1>
+                    <h1 className='home-title'>Be The Proud Of Nation</h1>
+                    <p className='w-75 text-start'> Britain's Rolls-Royce Motor Cars said on Monday that it enjoyed record annual sales of its luxury status symbol vehicles, as the broader sector grappled with pandemic fallout including a semiconductor shortage.</p>
+                    <button className='home-btn'>See More</button>
+                </div>
+                {/* home img  */}
+                <img className='home-img' src={img} alt="" />
+            </div>
+            {/* review part */}
+            <h1>review all</h1>
         </div>
     );
 };
